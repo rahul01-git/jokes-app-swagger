44fbcf3665f181f6e648ac8f304305f91fe198b6
rahul01-git <rahulneupane0090@gmail.com>
2024-09-09 20:18:12 +0545

    done

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require('cors')
 const { router: jokeRoutes } = require("./routes/joke.routes");
 const { authenticateDB } = require("./db-setup");
 const { swaggerDocs } = require("./swagger");
 
 const app = express();
 const port = process.env.PORT || 8080;
+const corsOptions = {
+  origin: 'http://localhost:8000',
+  optionsSuccessStatus: 200
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.disable("x-powered-by");
